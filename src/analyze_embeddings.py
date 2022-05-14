9554e81b9b9ce7f8c254e1282e91037c22871759
kevinbdsouza <kdsouza1496@gmail.com>
2022-05-14 16:15:19 -0700

    update

--- src/analyze_embeddings.py
+++ src/analyze_embeddings.py
@@ -83,9 +83,9 @@ def plot3d(representations, colors, labels):
     plt.figure()
     ax = plt.axes(projection='3d')
     for i in range(len(representations)):
-        ax.scatter3D(representations[i, 0], representations[i, 1], representations[i, 2], color=colors[i])
+        ax.scatter3D(representations[i, 0], representations[i, 1], representations[i, 2], color=colors[i], label=labels[i])
 
-    plt.legend(handles=ax.legend_elements()[0], labels=labels)
+    ax.legend()
     plt.show()
 
 
@@ -137,9 +137,10 @@ if __name__ == "__main__":
         sub_df = sub_df[sub_df != 0]
         rand = int(random.choice(sub_df.index))
         colors.append(cfg.colors_list[rand])
+        labels.append(cfg.class_elements_list[rand])
 
-    reduce_pca(embed_rows1, colors, cfg.class_elements_list)
-    reduce_pca(embed_rows2, colors, cfg.class_elements_list)
+    reduce_pca(embed_rows1, colors, labels)
+    reduce_pca(embed_rows2, colors, labels)
 
     #plot_smoothness(embed_rows1)
     #plot_smoothness(embed_rows2)
