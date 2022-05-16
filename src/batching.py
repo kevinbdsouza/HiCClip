98203886b093160c4e6a0a3c540befcaa098640f
kevinbdsouza <kdsouza1496@gmail.com>
2022-05-16 15:54:30 -0700

    update

--- src/batching.py
+++ src/batching.py
@@ -131,7 +131,7 @@ if __name__ == "__main__":
     cfg = Config()
     chr = 1
     batch_embed_ob = BatchHiCLSTMEmbeddings(cfg, chr)
-    batch_embed_input = batch_embed_ob.batch_embeddings(200)
+    batch_embed_input = batch_embed_ob.batch_embeddings(500)
 
     batch_hic_ob = BatchHiCMaps(cfg, chr)
-    batched_hic = batch_hic_ob.batch_hic_maps(200)
+    batched_hic = batch_hic_ob.batch_hic_maps(500)
