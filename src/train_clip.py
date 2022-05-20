75ccb16f94110d01b0e5b3e4b3f8460dacd1352f
kevinbdsouza <kdsouza1496@gmail.com>
2022-05-19 18:29:46 -0700

    update

--- src/train_clip.py
+++ src/train_clip.py
@@ -90,7 +90,7 @@ def train_clip(device, resume, cfg):
             batch_indices = np.random.permutation(pairpos_batched.shape[0])
 
             for batch_indice in batch_indices:
-                if chr == 21 and batch_indice == 48:
+                if (chr == 21 and batch_indice == 48) or (chr == 3 and batch_indice == 182):
                     continue
 
                 pairpos = np.array(pairpos_batched[batch_indice])
