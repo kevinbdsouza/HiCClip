2de64c8020ed4a144f290abbb930b13692d3df7b
kevinbdsouza <kdsouza1496@gmail.com>
2022-05-19 18:50:06 -0700

    update

--- src/train_clip.py
+++ src/train_clip.py
@@ -94,7 +94,8 @@ def train_clip(device, resume, cfg):
                         chr == 3 and (
                         batch_indice == 182 or batch_indice == 154 or batch_indice == 175 or
                         batch_indice == 136 or batch_indice == 302 or batch_indice == 4 or
-                        batch_indice == 488 or batch_indice == 774 or batch_indice == 341)):
+                        batch_indice == 488 or batch_indice == 774 or batch_indice == 341 or
+                        batch_indice == 40)):
                     continue
 
                 pairpos = np.array(pairpos_batched[batch_indice])
