7de74f509d9e45ca8868ed9b9f86b02e7f7de766
kevinbdsouza <kdsouza1496@gmail.com>
2022-05-26 13:23:29 -0700

    update

--- src/batching.py
+++ src/batching.py
@@ -23,6 +23,46 @@ class BatchFasta():
         pass
 
 
+class BatchIndices():
+    """
+    Class to Batch HiCLSTM Embeddings for Clip
+    """
+
+    def __init__(self, cfg, chr):
+        self.chr = chr
+        self.cfg = cfg
+        self.cumpos = get_cumpos(cfg, chr)
+        if chr == 22:
+            self.cumpos_next = cfg.genome_len
+        else:
+            self.cumpos_next = get_cumpos(cfg, chr + 1)
+
+    def batch_indices(self, batch_size):
+        seq_len = int(self.cfg.clip_config["text_seq_len"] / 2)
+        indices = np.arange(self.cumpos + 1, self.cumpos_next + 1)
+        fill_length = seq_len - (len(indices) % seq_len)
+
+        fill = np.zeros((fill_length,))
+        indices = np.concatenate((indices, fill))
+        num_seqs = int(len(indices) / seq_len)
+
+        indices_input = []
+        batched_indices = []
+        for r in range(num_seqs):
+            for c in range(num_seqs):
+                r_indices = indices[r * seq_len: (r + 1) * seq_len, :]
+                c_indices = indices[c * seq_len: (c + 1) * seq_len, :]
+
+                temp_indices = np.concatenate((r_indices, c_indices), axis=0)
+                indices_input.append(temp_indices)
+
+                if (len(indices_input) == batch_size) or (r == num_seqs - 1 and c == num_seqs - 1):
+                    batched_indices.append(indices_input)
+                    indices_input = []
+
+        return batched_indices
+
+
 class BatchHiCLSTMEmbeddings():
     """
     Class to Batch HiCLSTM Embeddings for Clip
@@ -43,7 +83,7 @@ class BatchHiCLSTMEmbeddings():
 
     def batch_embeddings(self, batch_size):
         embed_rows = self.load_embeddings()
-        embed_rows = embed_rows[self.cumpos + 1:self.cumpos_next]
+        embed_rows = embed_rows[self.cumpos:self.cumpos_next]
 
         seq_len = int(self.cfg.clip_config["text_seq_len"] / 2)
         fill_length = seq_len - (len(embed_rows) % seq_len)
@@ -135,8 +175,14 @@ if __name__ == "__main__":
         batched_embed = batch_embed_ob.batch_embeddings(cfg.clip_batch_size)
         np.save(cfg.batched_hic_path + "embed_%s.npy" % chr, batched_embed)
 
+        """
         batch_hic_ob = BatchHiCMaps(cfg, chr)
         batched_hic = batch_hic_ob.batch_hic_maps(cfg.clip_batch_size)
         np.save(cfg.batched_hic_path + "hic_%s.npy" % chr, batched_hic)
+        """
+
+        batch_ind_ob = BatchIndices(cfg, chr)
+        batched_indices = BatchIndices.batch_indices(cfg.clip_batch_size)
+        np.save(cfg.batched_hic_path + "indices_%s.npy" % chr, batched_indices)
 
     print("done")
