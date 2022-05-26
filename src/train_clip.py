b0894e6e73fb1bafda9d119013bc8dd400ea802c
kevinbdsouza <kdsouza1496@gmail.com>
2022-05-26 15:10:40 -0700

    update

--- src/train_clip.py
+++ src/train_clip.py
@@ -80,7 +80,8 @@ def train_clip(device, cfg):
         epoch_loss = []
         random.shuffle(cfg.chr_train_list_shuff)
         for chr in cfg.chr_train_list_shuff:
-            pairpos_batched = np.load(cfg.batched_hic_path + "embed_%s.npy" % chr, allow_pickle=True)
+            #pairpos_batched = np.load(cfg.batched_hic_path + "embed_%s.npy" % chr, allow_pickle=True)
+            pairpos_batched = np.load(cfg.batched_hic_path + "indices_%s.npy" % chr, allow_pickle=True)
             maps_batched = np.load(cfg.batched_hic_path + "hic_%s.npy" % chr, allow_pickle=True)
 
             batch_indices = np.random.permutation(pairpos_batched.shape[0])
