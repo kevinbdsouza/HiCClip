50d083b3fe34274f94757ba09facf777bd9e45e2
kevinbdsouza <kdsouza1496@gmail.com>
2022-05-30 13:04:38 -0700

    update

--- src/batching.py
+++ src/batching.py
@@ -43,22 +43,112 @@ class BatchIndices():
             self.cumpos_next = cfg.genome_len
         else:
             self.cumpos_next = get_cumpos(cfg, chr + 1)
+        self.seq_len = int(self.cfg.clip_config["text_seq_len"] / 2)
+
+    def modify_reps(self, chr_done, rep_chrs):
+        if len(chr_done) >= 17:
+            rep_chrs = 64
+        elif len(chr_done) >= 16:
+            rep_chrs = 32
+        elif len(chr_done) >= 14:
+            rep_chrs = 16
+        elif len(chr_done) >= 10:
+            rep_chrs = 8
+        elif len(chr_done) >= 8:
+            rep_chrs = 6
+        elif len(chr_done) >= 2:
+            rep_chrs = 4
+        else:
+            pass
+
+        return rep_chrs
+
+    def batch_chromo_init(self, chr):
+        self.chr = chr
+        self.cumpos = get_cumpos(self.cfg, chr)
+        if chr == 22:
+            self.cumpos_next = cfg.genome_len
+        else:
+            self.cumpos_next = get_cumpos(cfg, chr + 1)
+        indices = np.arange(self.cumpos + 1, self.cumpos_next + 1)
+        fill_length = self.seq_len - (len(indices) % self.seq_len)
+        fill = np.zeros((fill_length,))
+        indices = np.concatenate((indices, fill))
+        return indices
+
+    def get_partial_input(self, chr, indices_input, r_prev, c_prev, chr_done, rep_chrs, return_dict):
+        indices = self.batch_chromo_init(chr)
+        num_seqs = int(len(indices) / self.seq_len)
+
+        for i in range(0, rep_chrs):
+            if chr in chr_done:
+                continue
+            r_indices = indices[r_prev[chr - 1] * self.seq_len: (r_prev[chr - 1] + 1) * self.seq_len]
+            c_indices = indices[c_prev[chr - 1] * self.seq_len: (c_prev[chr - 1] + 1) * self.seq_len]
+
+            c_prev[chr - 1] += 1
+            if c_prev[chr - 1] == num_seqs:
+                c_prev[chr - 1] = 0
+                r_prev[chr - 1] += 1
+
+                if r_prev[chr - 1] == num_seqs:
+                    chr_done.append(chr)
+                    print("chr %s done" % chr)
+
+            temp_indices = np.concatenate((r_indices, c_indices), axis=0)
+            indices_input.append(temp_indices)
+
+        return_dict["indices_input"] = indices_input
+        return_dict["r_prev"] = r_prev
+        return_dict["c_prev"] = c_prev
+        return_dict["chr_done"] = chr_done
+
+    def batch_chromosome_wise(self):
+        num_chrs = 22
+        rep_chrs = 3
+        batch_num = 0
+
+        chr_done = []
+        r_prev = np.zeros((num_chrs)).astype(int)
+        c_prev = np.zeros(num_chrs).astype(int)
+
+        while len(chr_done) < 18 or batch_num < 2254:
+            batch_num += 1
+            indices_input = []
+            rep_chrs = self.modify_reps(chr_done, rep_chrs)
+            for chr in range(5, num_chrs + 1):
+                if chr in chr_done:
+                    continue
+
+                manager = mp.Manager()
+                return_dict = manager.dict()
+                proc = mp.Process(target=self.get_partial_input,
+                                  args=(chr, indices_input, r_prev, c_prev, chr_done, rep_chrs, return_dict))
+                proc.start()
+                proc.join()
+
+                indices_input = return_dict["indices_input"]
+                r_prev = return_dict["r_prev"]
+                c_prev = return_dict["c_prev"]
+                chr_done = return_dict["chr_done"]
+
+            np.save(cfg.cross_chromosome_batches + "cross_chr_ind_%s.npy" % batch_num, indices_input)
 
     def batch_indices(self, batch_size):
-        seq_len = int(self.cfg.clip_config["text_seq_len"] / 2)
+        self.seq_len = int(self.cfg.clip_config["text_seq_len"] / 2)
         indices = np.arange(self.cumpos + 1, self.cumpos_next + 1)
-        fill_length = seq_len - (len(indices) % seq_len)
+        fill_length = self.seq_len - (len(indices) % self.seq_len)
 
         fill = np.zeros((fill_length,))
         indices = np.concatenate((indices, fill))
-        num_seqs = int(len(indices) / seq_len)
+        num_seqs = int(len(indices) / self.seq_len)
 
         indices_input = []
         batched_indices = []
         for r in range(num_seqs):
             for c in range(num_seqs):
-                r_indices = indices[r * seq_len: (r + 1) * seq_len]
-                c_indices = indices[c * seq_len: (c + 1) * seq_len]
+                r_indices = indices[r * self.seq_len: (r + 1) * self.seq_len]
+                c_indices = indices[c * self.seq_len: (c + 1) * self.seq_len]
 
                 temp_indices = np.concatenate((r_indices, c_indices), axis=0)
                 indices_input.append(temp_indices)
@@ -263,8 +353,11 @@ if __name__ == "__main__":
     cfg = Config()
 
     if cfg.batch_chromosome_wise:
-        batch_hic_ob = BatchHiCMaps(cfg, 1)
-        batch_hic_ob.batch_chromosome_wise()
+        # batch_hic_ob = BatchHiCMaps(cfg, 1)
+        # batch_hic_ob.batch_chromosome_wise()
+
+        batch_ind_ob = BatchIndices(cfg, 1)
+        batch_ind_ob.batch_chromosome_wise()
     else:
         for chr in cfg.chr_train_list:
             """
